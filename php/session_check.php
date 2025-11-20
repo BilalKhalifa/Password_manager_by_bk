f42507748e4f6821e4b1bff79d1d91410f1a351a
BilalKhalifa <bilalkhalifa9988@gmail.com>
2025-11-20 17:49:40 +0530

    Remove PHP backend files; migrated to Node API

--- php/session_check.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-if (!isset($_SESSION['user_id'])) {
-    // If the user is not logged in, redirect to login page
-    header('Location: ../index.html');
-    exit();
-}
-?>
--- php/test_connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("db.php");
-if ($conn) {
-    echo "✅ Connected successfully to TiDB Cloud!";
-} else {
-    echo "❌ Connection failed!";
-}
